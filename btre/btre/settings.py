234fb7581584f05afeafaaa3e652b3611f4c42cf
LordCeguta <lordceguta@gmail.com>
2024-01-12 10:02:27 +0100

    TODO MODELS

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -128,6 +128,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
 
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
