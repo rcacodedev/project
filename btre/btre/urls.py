624cb7ee316218a53646463b621e852cd0659da9
LordCeguta <lordceguta@gmail.com>
2024-01-13 10:22:18 +0100

    Make search

--- btre/btre/urls.py
+++ btre/btre/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('listings/', include('listings.urls')),
+    path('search/', include('listings.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- btre/listings/views.py
+++ btre/listings/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Listing
+from listings.choices import state_choices, price_choices, bedroom_choices
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all().order_by('-list_date').filter(is_published=True)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context ={
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'listings/search.html', context)
