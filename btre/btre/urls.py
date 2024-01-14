f7f706ec016f635c29d3f47caa06e674e983157d
LordCeguta <lordceguta@gmail.com>
2024-01-14 09:57:59 +0100

    Inquiry

--- btre/btre/urls.py
+++ btre/btre/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('listings/', include('listings.urls')),
     path('search/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ btre/contacts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'listing', 'name', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 25
+
+admin.site.register(Contact, ContactAdmin)
